422fa5e4b3325291914ec7fab2b042648c879979
Jon Hallett <jon.hallett@bristol.ac.uk>
2017-07-06 16:27:55 +0100

    ...and add the new file

---
+++ manifests/reverseproxy/server.pp
@@ -0,0 +1,27 @@
+define existdb::reverseproxy::server (
+  $server_name = $title,
+  $server_cert_name = $server_name,
+  $ssl_cert = "/etc/pki/tls/certs/${server_cert_name}.crt",
+  $ssl_key = "/etc/pki/tls/private/${server_cert_name}.key",
+  $uri_path = '',
+  $proxy_redirect = 'default',
+) {
+  include nginx
+  nginx::resource::server { $server_name:
+    proxy            => "http://127.0.0.1:8080${uri_path}",
+    proxy_redirect   => $proxy_redirect,
+    ssl              => true,
+    ssl_redirect     => true,
+    ssl_cert         => $ssl_cert,
+    ssl_key          => $ssl_key,
+    proxy_set_header => [
+      'Host $host',
+      'X-Real-IP $remote_addr',
+      'X-Forwarded-For $proxy_add_x_forwarded_for',
+      'X-Forwarded-Host $host',
+      'X-Forwarded-Proto $scheme',
+      'Proxy ""',
+    ],
+    require          => Class['existdb'],
+  }
+}
